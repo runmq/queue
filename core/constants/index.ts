43677616e706037a73c94425d970fdf85e9a977e
Fawzi Abdulfattah <iifawzie@gmail.com>
2024-06-01 14:25:07 +0300

    feat: adding abstract producer with clinet connector interface
    Signed-off-by: Fawzi Abdulfattah <iifawzie@gmail.com>

---
+++ core/constants/index.ts
@@ -0,0 +1,10 @@
+/**
+ * A collection of constants used throughout the project.
+ */
+export const CONSTANTS = {
+  /**
+   * The prefix used for naming queues.
+   * @type {string}
+   */
+  QUEUE_PREFIX: "runmq:",
+};
---
+++ core/producer/index.ts
@@ -0,0 +1,67 @@
+import { IClientConnector } from "@core/types";
+
+/**
+ * AbstractProducer is an abstract class that provides basic functionalities for adding data to queues.
+ * It ensures atomic operations if needed and handles graceful shutdown on process termination signals.
+ */
+export default abstract class AbstractProducer<K> {
+  /**
+   * Creates an instance of AbstractProducer.
+   * @param name - The name of the queue.
+   * @param client - An instance of IClientConnector to interact with the queue.
+   */
+  constructor(
+    private name: string,
+    private client: IClientConnector<K>
+  ) {
+    process.once("SIGINT", this.handleShutdown.bind(this));
+    process.once("SIGTERM", this.handleShutdown.bind(this));
+  }
+
+  /**
+   * Adds data to the main queue and optionally to additional queues atomically.
+   * @param data - The data to be added to the queue.
+   * @param additionalQueues - Optional. Additional queues to which the data should be added atomically.
+   * @returns A promise that resolves when the data has been added.
+   */
+  public async add(data: any, additionalQueues?: string[]) {
+    await this.client.create();
+    if (additionalQueues && additionalQueues.length) {
+      await this.client.addAtomic(this.name, additionalQueues, data);
+      return;
+    }
+    await this.client.add(this.name, data);
+  }
+
+  /**
+   * Adds multiple data entries to the main queue and optionally to additional queues atomically.
+   * @param data - An array of data entries to be added to the queue.
+   * @param additionalQueues - Optional. Additional queues to which the data should be added atomically.
+   * @returns A promise that resolves when the data has been added.
+   */
+  public async bulk(data: any[], additionalQueues: string[] = []) {
+    await this.client.create();
+    if (additionalQueues.length) {
+      await this.client.bulkAtomic(this.name, additionalQueues, data);
+      return;
+    }
+    await this.client.bulk(this.name, data);
+  }
+
+  /**
+   * Disconnects the client from the queue.
+   * @returns A promise that resolves when the client has been disconnected.
+   */
+  public async disconnect() {
+    await this.client.quit();
+  }
+
+  /**
+   * Handles graceful shutdown by disconnecting the client.
+   * This method is bound to process termination signals (SIGINT, SIGTERM).
+   * @private
+   */
+  private async handleShutdown() {
+    await this.disconnect();
+  }
+}
