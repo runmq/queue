bf790b0ff4c925854df3b093f7596cff19cf73f5
Fawzi Essam <iifawzie@gmail.com>
2025-10-04 23:21:32 +0200

    remove useless jest pre/after calls
    Signed-off-by: Fawzi Essam <iifawzie@gmail.com>

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,9 +1,1 @@
-beforeAll(async () => {
-    console.log('Setting up test environment...');
-});
-
-afterAll(async () => {
-    console.log('Cleaning up test environment...');
-});
-
 jest.setTimeout(30000);
