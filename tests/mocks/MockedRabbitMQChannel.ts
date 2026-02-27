5dec913e6649f90b7116f15f194d63e8444b82c7
Fawzi Essam <iifawzie@gmail.com>
2026-02-27 03:48:35 +0200

    Fixing mock implementation
    Signed-off-by: Fawzi Essam <iifawzie@gmail.com>

--- tests/mocks/MockedRabbitMQChannel.ts
+++ tests/mocks/MockedRabbitMQChannel.ts
@@ -1,46 +1,24 @@
-import {Channel, Connection} from "amqplib";
+import {AMQPChannel} from "@src/types";
+import {Connection} from "rabbitmq-client";
 
-export class MockedRabbitMQChannel implements Channel {
-    unbindQueue = jest.fn();
+export class MockedRabbitMQChannel implements AMQPChannel {
     bindQueue = jest.fn();
-    purgeQueue = jest.fn();
     deleteQueue = jest.fn();
     checkQueue = jest.fn();
     assertQueue = jest.fn();
-    unbindExchange = jest.fn();
-    bindExchange = jest.fn();
     deleteExchange = jest.fn();
     checkExchange = jest.fn();
     assertExchange = jest.fn();
     publish = jest.fn();
-    sendToQueue = jest.fn();
     consume = jest.fn();
-    cancel = jest.fn();
     get = jest.fn();
     ack = jest.fn();
-    ackAll = jest.fn();
     nack = jest.fn();
-    nackAll = jest.fn();
-    reject = jest.fn();
     prefetch = jest.fn();
-    recover = jest.fn();
     close = jest.fn();
     connection: Connection = {} as Connection;
-    addListener = jest.fn();
     on = jest.fn();
-    once = jest.fn();
-    removeListener = jest.fn();
     off = jest.fn();
-    removeAllListeners = jest.fn();
-    setMaxListeners = jest.fn();
-    getMaxListeners = jest.fn();
-    listeners = jest.fn();
-    rawListeners = jest.fn();
-    emit = jest.fn();
-    listenerCount = jest.fn();
-    eventNames = jest.fn();
-    prependListener = jest.fn();
-    prependOnceListener = jest.fn();
 }
 
 export class MockedRabbitMQChannelWithAcknowledgeFailure extends MockedRabbitMQChannel {
